5ad26498b412ad921d246cc9995013cc41a71b5b
Juan Pablo Martínez Brenes <jpmb1220@gmail.com>
2018-09-05 10:46:17 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TareaCorta1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/juanpablomartinezbrenes/Documents/ITCR/CE-2103/TareaCorta1/main.cpp" "/Users/juanpablomartinezbrenes/Documents/ITCR/CE-2103/TareaCorta1/cmake-build-debug/CMakeFiles/TareaCorta1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TareaCorta1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TareaCorta1.dir/main.cpp.o"
+  "TareaCorta1.pdb"
+  "TareaCorta1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TareaCorta1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
